0abe7284fa9c9a8fd00b3cb640f2a27ad255f1cf
Ryan Gonzalez <rymg19@gmail.com>
2016-02-14 18:17:41 -0600

    Export some hidden functions

--- fluidsynth/src/midi/fluid_midi.h
+++ fluidsynth/src/midi/fluid_midi.h
@@ -247,11 +247,11 @@ struct _fluid_track_t {
 
 typedef struct _fluid_track_t fluid_track_t;
 
-fluid_track_t* new_fluid_track(int num);
+FLUIDSYNTH_API fluid_track_t* new_fluid_track(int num);
 int delete_fluid_track(fluid_track_t* track);
 int fluid_track_set_name(fluid_track_t* track, char* name);
 char* fluid_track_get_name(fluid_track_t* track);
-int fluid_track_add_event(fluid_track_t* track, fluid_midi_event_t* evt);
+FLUIDSYNTH_API int fluid_track_add_event(fluid_track_t* track, fluid_midi_event_t* evt);
 fluid_midi_event_t* fluid_track_first_event(fluid_track_t* track);
 fluid_midi_event_t* fluid_track_next_event(fluid_track_t* track);
 int fluid_track_get_duration(fluid_track_t* track);
@@ -309,8 +309,8 @@ struct _fluid_player_t {
   void* playback_userdata; /* pointer to user-defined data passed to playback_callback function */
 };
 
-int fluid_player_add_track(fluid_player_t* player, fluid_track_t* track);
-int fluid_player_callback(void* data, unsigned int msec);
+FLUIDSYNTH_API int fluid_player_add_track(fluid_player_t* player, fluid_track_t* track);
+FLUIDSYNTH_API int fluid_player_callback(void* data, unsigned int msec);
 int fluid_player_count_tracks(fluid_player_t* player);
 fluid_track_t* fluid_player_get_track(fluid_player_t* player, int i);
 int fluid_player_reset(fluid_player_t* player);
